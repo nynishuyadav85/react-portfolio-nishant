42bcb4606b045aef0b5d498af7df8bbffdad0519
Nishant Yadav <nynishuyadav85@gmail.com>
2025-03-09 21:23:55 +0530

    created a contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import About from './components/About'
+import Contact from './components/Contact'
 import Experience from './components/Experience'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
@@ -21,6 +22,7 @@ function App() {
         <Technologies />
         <Experience />
         <Project />
+        <Contact />
       </div>
 
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,5 @@
 import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
@@ -42,7 +40,6 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "NOIDA",
+  email: "nynishuyadav85@gmail.com",
 };
