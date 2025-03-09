493aaea32de141e10352969b7b32669614560424
Nishant Yadav <nynishuyadav85@gmail.com>
2025-03-09 10:04:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl font-bold underline'>React App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
