ebdf11d2b62151eb7003a0adcf5eecaf582d77c4
Nishant Yadav <nynishuyadav85@gmail.com>
2025-03-09 10:35:00 +0530

    created a navbar section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from '../assets/kevinRushLogo.png'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaTwitterSquare } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+
+const Navbar = () => {
+  return (
+    <nav className='flex items-center justify-between py-6'>
+      <div className='flex flex-shrink-0 items-center'>
+        <img className='mx-2 w-10' src={logo} alt='logo' />
+      </div>
+      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <FaGithub />
+        <FaLinkedin />
+        <FaTwitterSquare />
+        <FaInstagram />
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
