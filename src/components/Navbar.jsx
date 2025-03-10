82174ac1f51ef89837520a0823a29cb8c8300134
Nishant Yadav <nynishuyadav85@gmail.com>
2025-03-10 10:21:26 +0530

    added framer motion logic in project and contact sectionand added logic to for all the icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,17 +6,32 @@ import { FaTwitterSquare } from 'react-icons/fa'
 import { FaInstagram } from 'react-icons/fa'
 
 const Navbar = () => {
+  const handleGithubClick = () => {
+    window.open('https://github.com/nynishuyadav85', '_blank');
+  }
+
+  const handleLinkedinClick = () => {
+    window.open('https://www.linkedin.com/in/nishant-y-051428105/', '_blank');
+  }
+
+  const handleTwitterClick = () => {
+    window.open('https://x.com/nynishu15', '_blank');
+  }
+
+  const handleInstagramClick = () => {
+    window.open('https://www.instagram.com/nynishu2915/', '_blank');
+  }
+
   return (
     <nav className='flex items-center justify-between py-6'>
       <div className='flex flex-shrink-0 items-center'>
         <img className='mx-2 w-10' src={logo} alt='logo' />
       </div>
       <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <FaGithub />
-        <FaLinkedin />
-        <FaTwitterSquare />
-        <FaInstagram />
-
+        <FaGithub onClick={handleGithubClick} style={{ cursor: 'pointer' }} />
+        <FaLinkedin onClick={handleLinkedinClick} style={{ cursor: 'pointer' }} />
+        <FaTwitterSquare onClick={handleTwitterClick} style={{ cursor: 'pointer' }} />
+        <FaInstagram onClick={handleInstagramClick} style={{ cursor: 'pointer' }} />
       </div>
     </nav>
   )
